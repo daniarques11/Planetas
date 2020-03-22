5df4614de414f2e5d426500530d15733b0213aaa
unknown <darques@cifpfbmoll.eu>
2020-03-22 13:52:11 +0100

    feat: Acabar histories d'usuari

--- src/main/java/edu/elsmancs/planets/App.java
+++ src/main/java/edu/elsmancs/planets/App.java
@@ -12,17 +12,15 @@ public class App {
 			System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
 		}
 
-		// System.out.println("\nYour weight only on the terrestrial planets: ");
-		// for(Planeta planeta: Planeta.getPlanetasTerrestres()){
-		// System.out.printf("Your weight on %s is %f N%n", planeta.name(),
-		// planeta.pesoSuperficie(peso));
-		// }
+		System.out.println("\nYour weight only on the terrestrial planets: ");
+		for (Planeta planeta : Planeta.getTerrestralPlanets()) {
+			System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+		}
 
-		// System.out.println("\nYour weight only on the gas giant planets: ");
-		// for(Planeta planeta: Planeta.getGigantesGaseosos()){
-		// System.out.printf("Your weight on %s is %f N%n", planeta.name(),
-		// planeta.pesoSuperficie(peso));
-		// }
+		System.out.println("\nYour weight only on the gas giant planets: ");
+		for (Planeta planeta : Planeta.getGaseousPlanets()) {
+			System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+		}
 	}
 
 }
