40d69de116520982b010dce56780c9d84e553f4a
unknown <darques@cifpfbmoll.eu>
2020-03-22 13:06:25 +0100

    build: Canviar gestor de projectes a Maven per problema amb AssertJ

---
+++ src/main/java/edu/elsmancs/planets/App.java
@@ -0,0 +1,28 @@
+package edu.elsmancs.planets;
+
+//import org.foobarspam.arnoldEnumType.logica.Planeta;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		double peso = 175.0; // kg
+
+		for (Planeta planeta : Planeta.values()) {
+			System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+		}
+
+		// System.out.println("\nYour weight only on the terrestrial planets: ");
+		// for(Planeta planeta: Planeta.getPlanetasTerrestres()){
+		// System.out.printf("Your weight on %s is %f N%n", planeta.name(),
+		// planeta.pesoSuperficie(peso));
+		// }
+
+		// System.out.println("\nYour weight only on the gas giant planets: ");
+		// for(Planeta planeta: Planeta.getGigantesGaseosos()){
+		// System.out.printf("Your weight on %s is %f N%n", planeta.name(),
+		// planeta.pesoSuperficie(peso));
+		// }
+	}
+
+}
